8e0ccab099f8aa3ca15e2e7fb7069e43f9eb6d90
maridoroshuk <m.doroshuk@bk.ru>
2022-06-22 08:17:56 +0300

    feat: added _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import { SWRConfig } from "swr"
+import "tailwindcss"
+import { makeServer } from "../mirage"
+
+let isClient = typeof window !== "undefined"
+if (isClient && process.env.NODE_ENV === "development") {
+	if (!window.server) {
+		window.server = makeServer({ environment: "development" })
+	}
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div className="flex justify-center w-full min-h-screen pt-8 antialiased xs:px-20 sm:px-10 md:pt-28 bg-slate-100">
+			<SWRConfig
+				value={{
+					fetcher: (...args) => {
+						return isClient
+							? fetch(...args).then((res) => res.json())
+							: new Promise(() => {})
+					},
+					suspense: true
+				}}>
+				<div className="w-full max-w-4xl">
+					<Component {...pageProps} />
+				</div>
+			</SWRConfig>
+		</div>
+	)
 }
 
 export default MyApp
